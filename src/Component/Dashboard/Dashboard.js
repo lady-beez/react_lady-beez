306ee2c713aba8c4ccacad668ce38e06b7a26344
Xochil Squaglia <xochilsquaglia@yahoo.com>
2019-06-06 21:46:50 -0700

    pushing up current work

---
+++ src/Component/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+  render() {
+    return(
+      <section>
+        <p>Play the Spelling Bee Game</p>
+      </section>
+
+    );
+  }
+}
